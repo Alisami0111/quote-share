004d602c46028047ffcebfba56fc9208902a9ca1
alisami0111 <saminagri0111@gmail.com>
2024-06-24 19:32:21 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import './App.css';
+import React, {useState} from 'react';
+import {
+  FacebookShareButton,
+  FacebookIcon,
+} from "react-share";
+
+const App = () => {
+  const url = "https://api.quotable.io/random";
+  let quoteData = {
+    content: "Let time be your only competitor.",
+    author: "Ahmed Saber"
+  }
+  const [quote, setQuote] = useState(quoteData)
+
+  const generateQuote = () => {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setQuote(data)
+      });
+  }
+
+  const copy = () => {
+    navigator.clipboard.writeText(quote.author + " once said: " + quote.content)
+    alert('copied')
+  }
+
+  return (
+    <>
+    <h1>alisami</h1>
+      <h1>Quote Generator React App</h1>
+      <div className="container">
+        <p>{quote.content}</p>
+        <span>{quote.author}</span>
+        <div className="btns">
+          <button onClick={copy} className="btn">
+            Copy
+          </button>
+          <button onClick={generateQuote}>Generate Another Quote</button>
+        </div>
+      </div>
+      <FacebookShareButton url= "https://intern2grow.vercel.app/" title= {quote.content}>
+      <FacebookIcon size={40} />
+      </FacebookShareButton>
+    </>
+  );
+};
+
+export default App;
